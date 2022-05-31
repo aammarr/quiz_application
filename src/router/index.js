beeb2c5103bc6c0863c6709096b0cf118fb127a9
ammarahmed <aammarr.ahmed@gmail.com>
2022-05-31 19:29:49 +0500

    made female page

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/CarView.vue')
+    },
+    {
+      path: '/fashion',
+      name: 'Fashion',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/FashionView.vue')
     }
   ]
 })
--- src/views/FashionView.vue
+++ src/views/FashionView.vue
@@ -2,56 +2,104 @@
   <section class="custom-row">
     <div class="tw-flex tw-flex-column" style="margin: 0 0 60px 0">
       <header>
-        <img class="custom_logo" src="/src/assets/logos/logo_cars-4-sale.svg" />
+        <img class="custom_logo" src="/src/assets/logos/logo_fashion.svg" />
       </header>
 
-      <div
+      <h2 style="margin: 20px 0">For Girl</h2>
+
+      <div v-for="(row, index) in femaleJson" :key="`$fashion-{index}`">
+      <div 
         class="cards tw-mx-auto"
-        v-for="(cars, index) in myJson"
+        v-for="(femaleItems, subIndex) in row.data"
+        :key="femaleItems.id"
         :class="{ activeImg: activeIndex === index }"
-        @click="setActive(index)"
-        :key="cars.id"
+        @click="setActive(index, subIndex)"
+
       >
         <div class="estate_image" style="position: relative">
-
           <img
           v-if="activeIndex === index" 
           height="32" width="32" style="object-fit: contain; position: absolute; bottom: -60px;"
           src="/src/assets/icons/icon-circled-arrow.svg" >
           <img
             class="activeImg"
-            :src="`/src/assets/images/cars/${cars.thumbnail.filename}`"
+            style="object-fit: cover;"
+            :src="`/src/assets/images/fashion/female/${femaleItems.thumbnail.filename}`"
           />
         </div>
         <div class="estate_info">
           <div>
-            <h6>{{ cars.name }}</h6>
+            <h6>{{ femaleItems.name }}</h6>
             <img
               height="32"
               :src="
-                cars.options.agent.avatar.filename
-                  ? `/src/assets/images/avatars/${cars.options.agent.avatar.filename}`
+                femaleItems.options
+                  ? `/src/assets/images/avatars/${femaleItems.options.agent.avatar.filename}`
                   : '/src/assets/Image/user_placeholder.png'
               "
             />
-            <p class="name">{{ cars.options.agent.name || "Anonymous" }}</p>
+            <p class="name">{{ null || "Anonymous" }}</p>
           </div>
           <div>
             <p class="price">
-              ${{ cars.options.price.weekly_value || 0 }} <small>per week</small>
+              ${{ femaleItems.price || 0 }} <small>per week</small>
             </p>
           </div>
         </div>
+      </div>
+      </div>
+
+      <h2 style="margin: 20px 0">For Men</h2>
+
+      <div v-for="(row, index) in maleJson" :key="`$fashion-{index}`">
+      <div 
+        class="cards tw-mx-auto"
+        v-for="(maleItems, subIndex) in row.data"
+        :key="maleItems.id"
+        :class="{ activeImg: activeIndex === index }"
+        @click="setActive(index, subIndex)"
+
+      >
+        <div class="estate_image" style="position: relative">
+          <img
+          v-if="activeIndex === index" 
+          height="32" width="32" style="object-fit: contain; position: absolute; bottom: -60px;"
+          src="/src/assets/icons/icon-circled-arrow.svg" >
+          <img
+            class="activeImg"
+            style="object-fit: cover;"
+            :src="`/src/assets/images/fashion/male/${maleItems.thumbnail.filename}`"
+          />
+        </div>
+        <div class="estate_info">
+          <div>
+            <h6>{{ maleItems.name }}</h6>
+            <img
+              height="32"
+              :src="
+                maleItems.options
+                  ? `/src/assets/images/avatars/${maleItems.options.agent.avatar.filename}`
+                  : '/src/assets/Image/user_placeholder.png'
+              "
+            />
+            <p class="name">{{ null || "Anonymous" }}</p>
+          </div>
+          <div>
+            <p class="price">
+              ${{ maleItems.price || 0 }} <small>per week</small>
+            </p>
+          </div>
+        </div>
+      </div>
       </div>
     </div>
 
     <footer class="realEstate_footer">
       <ui-button
-        :class="{ carSale_btn: selectedCar }"
-        @click="$router.push('/fashion')"
-        :disabled="!selectedCar"
+        :class="{ fashionSale_btn: selectedFashion }"
+        :disabled="!selectedFashion"
         unelevated
-        >Select Car</ui-button
+        >Select Fashion</ui-button
       >
     </footer>
   </section>
@@ -63,23 +111,26 @@ export default {
   data() {
     return {
       activeIndex: null,
-      selectedCar: null,
+      selectedFashion: null,
     };
   },
   methods: {
-    setActive(index) {
+    setActive(index, subIndex) {
       this.activeIndex = index;
       // storing selected property.
-      this.selectedCar = this.myJson[index];
+      this.selectedFashion = this.femaleJson[index].data[subIndex];
     },
     getImagePath(name) {
       if (name) return `/src/assets/images/properties/` + name;
     },
   },
   computed: {
-    myJson() {
-      return jsonData.screens.carsForSale.data;
+    femaleJson() {
+      return jsonData.screens.fashion.female;
     },
+    maleJson() {
+      return jsonData.screens.fashion.male;
+    }
   },
 };
 console.log(jsonData);
@@ -104,12 +155,12 @@ console.log(jsonData);
   justify-content: center;
   align-items: center;
   text-align: center;
-  background-color: #e3f5f5;
+  background-color: #e99bcb;
 }
 
 .cards {
   width: 385px;
-  height: 365px;
+  max-height: 365px;
   margin: auto;
 }
 
@@ -192,8 +243,8 @@ console.log(jsonData);
   justify-content: center;
   align-items: center;
 }
-.carSale_btn {
-  background-color: #6cdada !important;
+.fashionSale_btn {
+  background-color: #971381 !important;
   border-radius: 10px;
   border: none;
   height: 40px;
