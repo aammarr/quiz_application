153a446fdd842708224bbcd6bc453ff99614a701
ammarahmed <aammarr.ahmed@gmail.com>
2022-05-31 08:39:27 +0500

    update files

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,19 @@ import { RouterLink, RouterView } from "vue-router";
 <template>
   <RouterView />
 </template>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+@font-face {
+  font-family: "Nunito";
+  src: url("./assets/fonts/Nunito/static/Nunito-Regular.ttf");
+}
+@font-face {
+  font-family: "Rubik";
+  src: url("./assets/fonts/Rubik/static/Rubik-Regular.ttf");
+}
+</style>
